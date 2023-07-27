83cad2d65374d14991a710bd6948c5f1b454c586
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-07-28 00:09:39 +0530

    rv unwanted

--- src/components/Home/Table.js
+++ src/components/Home/Table.js
@@ -64,7 +64,7 @@ export default function ColumnGroupingTable({ search, dateFilter }) {
   }, []);
 
   React.useEffect(() => {
-    if (search != "" && dateFilter !== null) {
+    if (search !== "" && dateFilter !== null) {
       setTableData(
         completeData.filter((ele) => {
           return (
