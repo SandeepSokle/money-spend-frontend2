e9861570f5de31ff77f8a667a196f947309ce3ab
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-08-03 02:03:47 +0530

    update link

--- src/components/functions/config.js
+++ src/components/functions/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080/api/2023/"
+// export const API_URL = "http://localhost:8080/api/2023/"
 
-// export const API_URL = "https://money-spends.onrender.com/api/2023/"
+export const API_URL = "https://money-spends.onrender.com/api/2023/"
