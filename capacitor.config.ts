4b5213c7ce2d9b30706d66e558c6ec86060ba8d8
SandeepSokle <sandeepsokle12@gmail.com>
2025-01-21 18:53:48 +0530

    update

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.moneyspend.moneyspend',
-  appName: 'moneyspend',
-  webDir: 'build'
-};
-
-export default config;
